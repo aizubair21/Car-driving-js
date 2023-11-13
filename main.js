a0a9c6853dba26c18a0310eb43c84806686dff3e
Amimul Ihsan Zubair <86657812+aizubair21@users.noreply.github.com>
2023-11-13 18:19:37 +0600

    Road lane
    -> Update with road lane.
    -> lane can change dynamic
    -> car sit on the middle of certain lane. by default last of lane.
    
    -> Next, we implement collusion between car and road left and right side.

--- main.js
+++ main.js
@@ -2,13 +2,12 @@
 const canvas = document.getElementById('carCanvas');
 canvas.width = 250; //canvas width 250px
 
-
 //get the 2D Drowing context to draw the car;
 const ctx = canvas.getContext("2d");
 
 //create a new object for the car. car take x position, y position, width and height
 const road = new Road(canvas.width / 2, canvas.width);
-const car = new Car(canvas.width / 2, 100, 20, 30);
+const car = new Car(road.getLineCenter(2), canvas.height, 20, 30);
 
 animate()
 
@@ -20,4 +19,4 @@ function animate() {
     requestAnimationFrame(animate)
     // requestAnimationFrame loop over and over in a second.
 }
-console.log(road.getLineCenter(0));
+// console.log(road.getLineCenter(3));
--- road.js
+++ road.js
@@ -1,13 +1,14 @@
 // road class 
 class Road {
     //constructoir properties.
-    constructor(x, width, laneCount = 4) { //lanecount default value of 3
-        this.x = x;
-        this.width = width;
+    constructor(x, width, laneCount = 5) {
+        //lanecount default value of 5
+        this.x = x; //x coordinate position
+        this.width = width; //width of canvas
         this.laneCount = laneCount;
 
-        this.left = 10;
-        this.right = width - 10;
+        this.left = x - width / 2;
+        this.right = x + width / 2;
 
         //road go intinite upwards
         const infinity = 1000000;
@@ -15,21 +16,37 @@ class Road {
         this.bottom = infinity;
     }
 
+    //get leane center point to set the car center of any specific lane. 
     getLineCenter(lineIndex) {
-        const lineWidth = this.width / this.laneCount;
-
-        return (`${this.left} + ${lineWidth} / 2 + ${lineIndex} * ${lineWidth} : ` + this.left + lineWidth / 2) + lineIndex * lineWidth;
+        const laneWidth = this.width / this.laneCount;
+        return this.left + laneWidth / 2 + Math.min(lineIndex, this.laneCount - 1) * laneWidth;
     }
 
-    Draw() {
-        ctx.lineWidth = 3;
-        ctx.strokeStyle = "white";
 
+    //lane draw method
+    Draw() {
+        ctx.lineWidth = 3; //3px width
+        ctx.strokeStyle = "white"; //whtie color lane
+
+        //loop of lane count to draw lane
         for (let i = 0; i <= this.laneCount; i++) {
+            /*
+             * lerp() a function defined bellow out of Road class
+             * x value depend on three things left and right position of road
+             * and current index of i devided by laneCount.
+             * this is basically make stapes of lane.
+             */
             const x = lerp(this.left, this.right, i / this.laneCount);
             if (i > 0 && i < this.laneCount) {
+                /*
+                 * setLineDash() a function from context object which used to create line dash in canvas.
+                 * first parameter is array of number that represent length of each dashes.
+                 * second parameter is offset of start of pattern.
+                 * it create line dash to lane. not include first and last lane.
+                 */
                 ctx.setLineDash([30, 20])
             } else {
+
                 ctx.setLineDash([]);
             }
             ctx.beginPath();
